b87d591c6349217807a435bac2b01f9e69cc141a
Aaron Ibarra <aaronibarra132@gmail.com>
2023-02-28 11:30:54 -0800

    successfully passing data into child component Zodiac

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './Main.css';
 import { zodiac } from '../../data.js';
+import Zodiac from '../Zodiac/Zodiac.js';
 
 export default function Main() {
-  return <></>;
+  return (
+    <main>
+      {zodiac.map((sign) => (
+        <Zodiac key={sign.id} {...sign} />
+      ))}
+    </main>
+  );
 }
---
+++ src/components/Zodiac/Zodiac.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Zodiac.css';
+
+export default function Zodiac(sign) {
+  return <div>{sign.name}</div>;
+}
