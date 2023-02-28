a7ecb9af283b3a2ecdcbd54a069dbd4006a6bf3f
Aaron Ibarra <aaronibarra132@gmail.com>
2023-02-28 11:57:31 -0800

    added styling to separate component css files

--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -3,7 +3,7 @@ import './Zodiac.css';
 
 export default function Zodiac(sign) {
   return (
-    <div>
+    <div className="zodiac-sign">
       <img src={`/signs/${sign.name}.svg`} />
       <h1>{sign.name}</h1>
       <p>{sign.dates}</p>
